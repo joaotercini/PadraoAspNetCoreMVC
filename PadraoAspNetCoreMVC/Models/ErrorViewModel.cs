25d6697101def7a7974d67c528723371eca5d529
Joao Tercini <joao.tercini@usp.br>
2020-07-28 23:16:50 -0300

    Improved from AspNet Core Empty to MVC template

---
+++ PadraoAspNetCoreMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace PadraoAspNetCoreMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
